f443b0179552a433fd298df4ad7cd00484c6b0d5
Sergey <klimser@inbox.ru>
2018-11-15 00:09:12 +0300

    Fix

--- frontend/views/site/error.php
+++ frontend/views/site/error.php
@@ -11,17 +11,22 @@ $this->title = $name;
 ?>
 <div class="site-error">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <?php if ($exception->statusCode == 404): ?>
+        <h1>Приносим извинения, но данная страница не существует.</h1>
 
-    <div class="alert alert-danger">
-        <?= nl2br(Html::encode($message)) ?>
-    </div>
 
-    <p>
-        The above error occurred while the Web server was processing your request.
-    </p>
-    <p>
-        Please contact us if you think this is a server error. Thank you.
-    </p>
+    <?php else: ?>
+        <h1><?= Html::encode($this->title) ?></h1>
 
+        <div class="alert alert-danger">
+            <?= nl2br(Html::encode($message)) ?>
+        </div>
+
+        <p>
+            The above error occurred while the Web server was processing your request.
+        </p>
+        <p>
+            Please contact us if you think this is a server error. Thank you.
+        </p>
+    <?php endif; ?>
 </div>
