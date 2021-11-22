a3f29beeb1776c3f39cefcacbceaf28d93824c57
Sergey Klimov <klimser2@gmail.com>
2021-11-22 15:20:43 +0100

    Fix

--- frontend/views/payment/index-new.php
+++ frontend/views/payment/index-new.php
@@ -74,7 +74,7 @@ $this->params['breadcrumbs'][] = 'Онлайн оплата для новых с
             </div>
             <div id="message_board"></div>
             <div class="text-right mw-100">
-                <?php if (Yii::$app->request->getCookies()->get('apelsin_test') == 'mfp45'): ?>
+                <?php if ('mfp45' == $_COOKIE['apelsin_test'] ?? null): ?>
                     <button type="submit" onclick="return Payment.completePayment(this);" data-payment="<?= Contract::PAYMENT_TYPE_APELSIN; ?>" class="btn apelsin_logo pay_button m-2">
                         Оплатить через <span class="sr-only">Apelsin</span> <i class="ml-2"></i>
                     </button>
