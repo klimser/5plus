3b55b702f7c4333a99d6d22839c6188a80378a37
Sergey Klimov <klimser2@gmail.com>
2021-11-24 17:16:31 +0100

    Apelsin enabled for all

--- frontend/views/payment/index-new.php
+++ frontend/views/payment/index-new.php
@@ -74,11 +74,9 @@ $this->params['breadcrumbs'][] = 'Онлайн оплата для новых с
             </div>
             <div id="message_board"></div>
             <div class="text-right mw-100">
-                <?php if ('mfp45' == ($_COOKIE['apelsin_test'] ?? null)): ?>
-                    <button type="submit" onclick="return Payment.completePayment(this);" data-payment="<?= Contract::PAYMENT_TYPE_APELSIN; ?>" class="btn apelsin_logo pay_button m-2">
-                        Оплатить через <span class="sr-only">Apelsin</span> <i class="ml-2"></i>
-                    </button>
-                <?php endif; ?>
+                <button type="submit" onclick="return Payment.completePayment(this);" data-payment="<?= Contract::PAYMENT_TYPE_APELSIN; ?>" class="btn apelsin_logo pay_button m-2">
+                    Оплатить через <span class="sr-only">Apelsin</span> <i class="ml-2"></i>
+                </button>
                 <button type="submit" onclick="return Payment.completeNewPayment(this);" data-payment="<?= Contract::PAYMENT_TYPE_ATMOS; ?>" class="btn atmos_logo pay_button m-2">
                     Оплатить через <span class="sr-only">Atmos</span> <i class="ml-2"></i>
                 </button>
