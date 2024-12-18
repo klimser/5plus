e8f70221e0c0533bad7dc7c658dd034c1c6dea01
Sergei Klimov <klimser2@gmail.com>
2024-12-19 00:49:12 +0100

    Bot subdomain for Telegram webhook

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -4,3 +4,4 @@ Yii::setAlias('frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('console', dirname(dirname(__DIR__)) . '/console');
 Yii::setAlias('external', dirname(dirname(__DIR__)) . '/external');
+Yii::setAlias('bot', dirname(dirname(__DIR__)) . '/bot');
