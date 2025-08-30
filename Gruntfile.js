f9f69906e42d00211deb89e4b2c5043a27b0c2ab
Sergei Klimov <klimser2@gmail.com>
2025-08-31 00:39:02 +0200

    Grunt, composer

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 module.exports = function(grunt) {
-    grunt.loadNpmTasks('grunt-contrib-sass');
+    // RIP sass TODO: implement something that works
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-uglify');
